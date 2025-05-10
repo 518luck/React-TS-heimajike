81fbc4d016ce6fa65100a3a4ef4deeef9c65767c
蜻蜓ink队长 <1512013298@qq.com>
2025-05-10 19:41:09 +0800

    测试antd-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { Button } from 'antd-mobile'
 const App = () => {
-  return <div>this is app</div>
+  return (
+    <div>
+      this is app
+      <hr />
+      <Button color='success'>按钮</Button>
+    </div>
+  )
 }
 export default App
