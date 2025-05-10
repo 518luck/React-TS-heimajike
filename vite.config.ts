c20cab7d86b79f2c03347250ebf274c1bdca8420
蜻蜓ink队长 <1512013298@qq.com>
2025-05-10 20:56:17 +0800

    配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+// path 是 Node.js 的核心模块，浏览器默认不支持需要
+// 所以需要安装一个 yarn add @types/node --dev
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    //resolve 专门做解析配置
+    alias: {
+      //alias 别名选项
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
