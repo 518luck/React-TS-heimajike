c438f7f13913165ef7a8aced438af1decaab916b
蜻蜓ink队长 <1512013298@qq.com>
2025-05-11 12:12:04 +0800

    文章列表界面与数据

---
+++ src/pages/Home/HomeList/index.tsx
@@ -0,0 +1,63 @@
+import { Image, List } from 'antd-mobile'
+import { useEffect, useState } from 'react'
+// mock数据
+import { fetchArticlesAPI } from '@/apis/list'
+import type { ListRes } from '@/apis/list'
+
+type Props = {
+  channelId: string
+}
+
+const HomeList = (props: Props) => {
+  const { channelId } = props
+  const [listRes, setListRes] = useState<ListRes>({
+    results: [],
+    pre_timestamp: '' + new Date().getTime(),
+  })
+
+  useEffect(() => {
+    // 调用接口
+    const getList = async () => {
+      try {
+        const res = await fetchArticlesAPI({
+          channel_id: channelId,
+          timestamp: '' + new Date().getTime(),
+        })
+        setListRes({
+          results: res.data.data.results,
+          pre_timestamp: res.data.data.pre_timestamp,
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getList()
+  }, [channelId])
+
+  return (
+    <>
+      <List>
+        {listRes.results.map((item) => (
+          <List.Item
+            key={item.art_id}
+            prefix={
+              item?.cover?.images && item.cover.images.length > 0 ? (
+                <Image
+                  src={item.cover.images[0]}
+                  style={{ borderRadius: 20 }}
+                  fit='cover'
+                  width={40}
+                  height={40}
+                />
+              ) : null
+            }
+            description={item.pubdate}>
+            {item.title}
+          </List.Item>
+        ))}
+      </List>
+    </>
+  )
+}
+
+export default HomeList
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,23 @@
 import { Tabs } from 'antd-mobile'
-import { useEffect, useState } from 'react'
 
 import './style.css'
-import type { ChannelItem } from '@/apis/list'
-import { fetchChannelAPI } from '@/apis/list'
+import { useTabs } from './useTabs.ts'
+import HomeList from './HomeList/index.tsx'
 
 const Home = () => {
-  const [channels, setChannels] = useState<ChannelItem[]>([])
+  const { channels } = useTabs()
 
-  useEffect(() => {
-    const getChannels = async () => {
-      try {
-        const res = await fetchChannelAPI()
-        setChannels(res.data.data.channels)
-      } catch (error) {
-        console.log(error)
-        throw new Error('获取频道列表失败')
-      }
-    }
-    getChannels()
-  }, [])
   return (
     <>
       <div className='tobContainer'>
         <Tabs>
-          <Tabs.Tab title='水果' key='fruits'>
-            菠萝
-          </Tabs.Tab>
-          <Tabs.Tab title='蔬菜' key='vegetables'>
-            西红柿
-          </Tabs.Tab>
-          <Tabs.Tab title='动物' key='animals'>
-            蚂蚁
-          </Tabs.Tab>
+          {channels.map((item) => {
+            return (
+              <Tabs.Tab title={item.name} key={item.id}>
+                <HomeList channelId={'' + item.id}></HomeList>
+              </Tabs.Tab>
+            )
+          })}
         </Tabs>
       </div>
     </>
